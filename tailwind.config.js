703338d780e65f5734e823599884dfb4b76defff
ADE YOGA WIYADNYANA <adeyogawiyadnyana@stikom-bali.ac.id>
2023-05-06 01:59:44 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './adoptme/index.html',
-    './adoptme/.js/**/*.js',
-  ],
+  content: ['./public/**/*.{html,js}'],
   theme: {
     extend: {
       fontFamily: {
