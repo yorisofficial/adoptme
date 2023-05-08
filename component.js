86fdd1c489ef4ea64fa49c21b339e8febc355033
ADE YOGA WIYADNYANA <adeyogawiyadnyana@stikom-bali.ac.id>
2023-05-09 02:53:42 +0800

    Update fitur
    Penambahan random dan display hewan yang akan di adoptsi

--- component.js
+++ component.js
@@ -1,30 +1,70 @@
 // Menentukan jumlah perulangan
 const numCards = 5;
 
+// Memilih elemen dengan id "O-content"
 const oContent = document.getElementById("O-content");
 
+// Looping sebanyak numCards
 for (let i = 0; i < numCards; i++) {
+  // Membuat elemen div baru dengan class "card" dan "shrink-0"
   const card = document.createElement("div");
   card.classList.add("card", "shrink-0", "bg-gray-50", "rounded-xl", "hover:drop-shadow-2xl");
+  card.id = "card"; // Menambahkan id untuk setiap elemen
 
+  // Menambahkan isi dari elemen div
   card.innerHTML = `
-    <div class="px-5">
-      <div class="relative">
-        <img src="http://source.unsplash.com/300x400/?animals" alt="gambar" class="mt-5 rounded-xl">
-        <div id="layer-card" class="hidden absolute h-full w-full top-0 left-0 bg-gray-900 bg-opacity-50 rounded-xl">
-          <a href="" class="absolute bg-primary text-gray-50 px-6 py-3 rounded-xl top-1/2 left-1/2 -translate-x-1/2">Adopt</a>
+    <div id="card" class="card-animal shrink-0 bg-gray-50 rounded-xl hover:drop-shadow-2xl">
+    <div class="content-card px-5">
+        <div class="relative">
+            <img src="http://source.unsplash.com/300x400/?dogs" alt="gambar" class="mt-5 rounded-xl">
+            <div id="layer-card" class="hidden absolute h-full w-full top-0 left-0 bg-gray-900 bg-opacity-50 rounded-xl">
+                <a href="#section-stories" class="absolute bg-pirmatext-primaryText bg-primary text-gray-50 px-6 py-3 rounded-xl top-1/2 left-1/2 -translate-x-1/2" data-usia="2 Tahun" data-berat="25 Kg">More Details</a>
+            </div>
         </div>
-      </div>
     </div>
     <div class="my-2 text-center">
-      <h1>Lorem, ipsum dolor.</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
+        <h1 id="nama">Shagy Dog</h1>
+        <p>post <span class="random"></span> week ago</p>
+      </div>
     </div>
   `;
 
+  // Menambahkan elemen baru ke dalam elemen dengan id "O-content"
   oContent.appendChild(card);
 }
 
+// detail data
+// Ambil elemen HTML yang diperlukan
+const namaElement = document.getElementById("nama");
+const buttonElement = document.getElementById("getDataButton");
+const namaInfoElement = document.getElementById("info-nama");
+const usiaInfoElement = document.getElementById("info-usia");
+const beratInfoElement = document.getElementById("info-berat");
+
+// Buat fungsi untuk mengambil data dan menampilkan informasi
+function getData() {
+  const nama = namaElement.innerText;
+  const usia = buttonElement.dataset.usia;
+  const berat = buttonElement.dataset.berat;
+
+  namaInfoElement.innerText = `Nama: ${nama}`;
+  usiaInfoElement.innerText = `Usia: ${usia}`;
+  beratInfoElement.innerText = `Berat: ${berat}`;
+}
+
+// Function untuk menghasilkan random number dari 1-10 untuk usia dan berat dan memperbarui atribut data pada elemen anchor
+function displayRandomData(event) {
+  event.preventDefault();
+  const usia = Math.floor(Math.random() * 10) + 1;
+
+  buttonElement.dataset.usia = `${usia} Tahun`;
+  getData();
+}
+
+buttonElement.addEventListener("click", getData);
+
+
+// JavaScript count up
 const count1 = document.getElementById("count-1");
 const count2 = document.getElementById("count-2");
 const count3 = document.getElementById("count-3");
@@ -80,9 +120,19 @@ function startCountUp() {
   }
 }
 
+// Jalankan fungsi startCountUp setiap kali halaman di-scroll
 window.addEventListener("scroll", startCountUp);
 
+function displayRandomNumber(element) {
+  const randomNumber = Math.floor(Math.random() * 10) + 1;
 
+  element.innerHTML = `${randomNumber}`;
+}
+
+const elements = document.querySelectorAll(".random");
+elements.forEach((element) => {
+  displayRandomNumber(element);
+});
 
 
 
